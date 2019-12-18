8af52da63681e53c44e80aae4877c76dd50657a7
TeoChoi <cn.teochoi@gmail.com>
2019-12-18 11:50:29 +0800

    代码优化

--- dotenv/confighelper.php
+++ dotenv/confighelper.php
@@ -1,24 +1,8 @@
 <?php
 
-class Config
-{
-    private static $items = null;
-
-    private function __construct(){}
-
-    public static function get($name, $default)
-    {
-        if (self::$items == null) {
-            self::$items = parse_ini_file(CONFIG_FILE, false, INI_SCANNER_TYPED);
-        }
-
-        return self::$items[$name] ?? $default;
-    }
-}
-
 if (! function_exists('config')) {
     function config($name, $default = null)
     {
-        return Config::get($name, $default);
+        return \yii\dotenv\Config::get($name, $default);
     }
 }
