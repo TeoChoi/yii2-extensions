84c6a6abb28fa1a37ad197f40f04da845b535777
TeoChoi <cn.teochoi@gmail.com>
2019-12-23 16:21:31 +0800

    暂存

--- helpers.php
+++ helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Dotenv\Dotenv;
+use yii\web\Response;
 
 (new Dotenv(RDC_CONFIG_PATH, 'rdc_security_config.properties'))->load();
 
@@ -40,7 +41,55 @@ if (!function_exists('env')) {
 }
 
 if (!function_exists('http')) {
-    function http($baseUri) {
+    function http($baseUri)
+    {
         return new \yii2\http\HttpClient($baseUri);
+    }
+}
+
+if (!function_exists('response_ok')) {
+    /**
+     * @param $message
+     * @param null $data
+     * @return \yii\console\Response|Response
+     */
+    function response_ok($message, $data = null)
+    {
+        return response_send(200, $message, $data);
+    }
+}
+
+if (!function_exists('response_fail')) {
+    /**
+     * 错误
+     * @param $message
+     * @param null $data
+     * @return \yii\console\Response|Response
+     */
+    function response_fail($message, $data = null)
+    {
+        return response_send(400, $message, $data);
+    }
+}
+
+if (!function_exists('response_send')) {
+    /**
+     * @param $code
+     * @param $message
+     * @param null $data
+     * @return \yii\console\Response|Response
+     */
+    function response_send($code, $message, $data = null)
+    {
+        $response = \Yii::$app->getResponse();
+        $response->format = Response::FORMAT_JSON;
+
+        $response->data = [
+            'code' => $code,
+            'message' => $message,
+            'data' => $data
+        ];
+
+        return $response;
     }
 }
